95176c6193b73404cbeee1fe89ece6eeaa514ff8
Yu Li <yu.li@tecsys.com>
2022-10-07 09:20:01 -0400

    Final completed SPI

--- currency-exchange-rate-app/src/main/java/module-info.java
+++ currency-exchange-rate-app/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module currency.exchange.rate.app {
 
     requires currency.exchange.rate.api;
-
     requires spring.boot;
     requires spring.boot.autoconfigure;
+
+    uses spi.currency.rate.service.api.ExchangeRateProvider;
 }
